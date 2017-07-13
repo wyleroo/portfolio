2403ebbaa1ad1991933619e5172fbeaea889d356
Wyatt Robinson <thewyatt@gmail.com>
2017-07-13 13:31:25 -0700

    Generated controllers for projects, resume, and photography. Index pages not complete. Links in navbar.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  get 'projects/index'
+  get 'resume/index'
+  get 'photography/index'
+
   root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/photography_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PhotographyControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
